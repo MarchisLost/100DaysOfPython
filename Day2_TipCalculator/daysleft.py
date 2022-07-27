c84c5f57aff95d9ee2b1c5520114676f6b5d6a46
David Pinto <davidpinto15@gmail.com>
2022-07-27 20:21:29 +0100

    added Day2

---
+++ Day2_TipCalculator/daysleft.py
@@ -0,0 +1,9 @@
+age = int(input("What is your current age?"))
+
+ageleft = 90-age
+daysleft = ageleft*365
+weeksleft = ageleft*52
+monthsleft = ageleft*12
+
+print(f"You have {daysleft} days, {weeksleft} weeks and {monthsleft} months left.")
+
