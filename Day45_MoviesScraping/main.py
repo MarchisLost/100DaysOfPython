c9f5b17e16da7b06fa59e2a72d773157039169e8
MarchisLost <davidpinto15@gmail.com>
2025-06-06 00:33:11 +0100

    max upvotes code

--- Day45_MoviesScraping/main.py
+++ Day45_MoviesScraping/main.py
@@ -6,11 +6,16 @@ soup = BeautifulSoup(res.text, 'html.parser')
 
 post_title = soup.find_all(class_="titleline")
 
+articles_texts = []
+articles_links = []
 for i in post_title:
-    print(i.text)
-    print(i.a['href'])
+    articles_texts.append(i.text)
+    articles_links.append(i.a['href'])
 
-upvote_number = soup.find_all(class_="score")
+upvote_number = [int(i.text.split()[0]) for i in soup.find_all(class_="score")]
 
-for i in upvote_number:
-    print(i.text)
+max_upvote = max(upvote_number)
+max_upvote_index = upvote_number.index(max_upvote)
+print(articles_texts[max_upvote_index])
+print(articles_links[max_upvote_index])
+print(upvote_number[max_upvote_index])
